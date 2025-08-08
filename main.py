de8a247450564fdc1a87f81ee6e6152391486212
Guru01100101 <madness.monk93@gmail.com>
2025-08-08 11:05:58 +0300

    add docstrings, correct pyproject.toml

--- main.py
+++ main.py
@@ -20,21 +20,25 @@ SPAM_PATTERN = re.compile(r'([рp][уy]бл[еe][йия]?)|([уy]д[аa]л[еe]�
 
 @dp.message()
 async def handle_all_messages(message: types.Message):
-    """_summary_
-
-    Args:
-        message (types.Message): _description_
+    """
+    Habdler for all messages in all chats.
+    If message contains spam, it will be deleted and user will be banned for 30 days.
+    If user is admin or owner message will be deleted only (without banning)
     """
     if message.text:
         if SPAM_PATTERN.search(message.text):
             try:
+                # First delete message, then get chat member status
                 await message.delete()
                 print(f"Deleted message from {message.from_user.username}: {message.text}")
+                
+                # Get chat member status
                 chat_member = await bot.get_chat_member(
                     chat_id=message.chat.id,
                     user_id=message.from_user.id
                 )
                 
+                # Check if user is admin or owner
                 if chat_member.status not in [ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.CREATOR]:
                     await bot.ban_chat_member(
                         chat_id=message.chat.id,
