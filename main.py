b5a326515479260d9f2ba69d50e841107ae3b228
Guru01100101 <madness.monk93@gmail.com>
2025-08-08 10:58:43 +0300

    feat: MVP bot with basic spam filtering

---
+++ main.py
@@ -0,0 +1,58 @@
+from datetime import datetime, timedelta
+import os
+import re
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.enums.chat_member_status import ChatMemberStatus
+from dotenv import load_dotenv
+
+# Dovnolad enviroment variables from .env file
+load_dotenv()
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+# Initialize bot and dispatcher
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+# regular expression for spam message
+# Find words "рубль", "рубли", "рублей" in message with latin and cyrillic letters
+SPAM_PATTERN = re.compile(r'([рp][уy]бл[еe][йия]?)|([уy]д[аa]л[еe]нн[оo])|(₽)|(\$)', re.IGNORECASE)
+
+@dp.message()
+async def handle_all_messages(message: types.Message):
+    """_summary_
+
+    Args:
+        message (types.Message): _description_
+    """
+    if message.text:
+        if SPAM_PATTERN.search(message.text):
+            try:
+                await message.delete()
+                print(f"Deleted message from {message.from_user.username}: {message.text}")
+                chat_member = await bot.get_chat_member(
+                    chat_id=message.chat.id,
+                    user_id=message.from_user.id
+                )
+                
+                if chat_member.status not in [ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.CREATOR]:
+                    await bot.ban_chat_member(
+                        chat_id=message.chat.id,
+                        user_id=message.from_user.id,
+                        until_date=datetime.now() + timedelta(days=30)
+                    )
+                    print(f"Banned user {message.from_user.username}")
+                else:
+                    print(f"User {message.from_user.username} is {chat_member.status.value}")
+            except Exception as e:
+                print(f"Error deleting message or banning user: {e}")
+                
+async def main():
+    """main function to start bot"""
+    print("Bot is starting...")
+    await dp.start_polling(bot)
+    print("Bot is stopped")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
